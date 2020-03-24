4afa2792c7401d39c8834d5cfd51f9be5c876d4e
Jared <jared@digitalnutt.com>
2020-03-24 09:58:47 -0700

    added variable

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const { REACT_APP_TEST } = process.env;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>Variable: {REACT_APP_TEST}</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
